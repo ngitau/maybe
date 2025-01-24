8b49134215208b32603c7d8fd3bea9ff2a90df0f
Joseph G. <joseph@ndungugitau.com>
2025-01-24 14:30:22 +0300

    Create production_setup.sql

---
+++ db/production_setup.sql
@@ -0,0 +1,6 @@
+-- db/production_setup.sql
+
+CREATE DATABASE maybe_production;
+-- CREATE DATABASE maybe_production_cache;
+-- CREATE DATABASE maybe_production_queue;
+-- CREATE DATABASE maybe_production_cable;
